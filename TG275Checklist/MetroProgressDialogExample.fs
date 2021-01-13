1bfdc76e062c25f80e517ddea52d166c309622f3
Ben Catt <bcatt09@gmail.com>
2021-01-13 12:25:00 -0500

    working PatientSetup Screen

---
+++ TG275Checklist/MetroProgressDialogExample.fs
@@ -0,0 +1,30 @@
+﻿module MetroProgressDialog
+
+open MahApps.Metro.Controls.Dialogs
+open TG275Checklist.Views
+open System.Threading
+
+let showDialog (window:MainWindow) =
+
+    // Register DataContext for displaying Metro Dialogs
+    DialogParticipation.SetRegister(window, window.DataContext)
+
+    async {
+        // Dialog Settings
+        let mySettings = 
+            MetroDialogSettings(
+                NegativeButtonText = "Cancel",
+                AnimateShow = false,
+                AnimateHide = false,
+                ColorScheme = MetroDialogColorScheme.Accented)
+            
+        // Display ProgressDialog
+        let! controller = DialogCoordinator.Instance.ShowProgressAsync (context = window.DataContext, title = "Logging in to Eclipse", message = "Please wait...", settings = mySettings) |> Async.AwaitTask
+        controller.SetIndeterminate()
+
+        // Do long calculation
+        do Thread.Sleep(5000)
+
+        // Close Dialog
+        do! controller.CloseAsync() |> Async.AwaitTask
+    }
--- TG275Checklist/Model.fs
+++ TG275Checklist/Model.fs
@@ -6,10 +6,14 @@ open Elmish.WPF
 open Esapi
 open TG275Checklist.Views
 open VMS.TPS.Common.Model.API
-open MahApps.Metro.Controls.Dialogs
 
-module PatientSetup =
-    type FlagType =
+module ChecklistOptions =
+    type Plan =
+        {
+            PlanId: string
+            CourseId: string
+        }
+    type ToggleType =
         | PreviousRT
         | Pacemaker
         | Registration
@@ -18,15 +22,26 @@ module PatientSetup =
         | IMRT
         | SRS
         | SBRT
-    type Flag =
+    
+    type ChecklistOptions =
         {
-            Type: FlagType
+            Plans: Plan list
+            Toggles: ToggleType list
+        }
+    let emptyChecklistOptions = { Plans = []; Toggles = [] }
+
+module PatientSetup =
+    open ChecklistOptions
+    // Patient setup toggles to be used in PatientSetup Screen
+    type Toggle =
+        {
+            Type: ToggleType
             Text: string
-            Checked: bool
+            IsChecked: bool
             Id: int
         }
-    let private getText flagType =
-        match flagType with
+    let private getText toggleType =
+        match toggleType with
         | PreviousRT _ -> "Previous RT"
         | Pacemaker _ -> "Pacemaker/ICD"
         | Registration _ -> "Registration"
@@ -35,93 +50,151 @@ module PatientSetup =
         | IMRT _ -> "IMRT/VMAT"
         | SRS _ -> "SRS"
         | SBRT _ -> "SBRT"
-    let flagList =  
+    let toggleList = 
         [
             {
                 Type = PreviousRT;
                 Text = getText PreviousRT;
-                Checked = false; 
+                IsChecked = false; 
                 Id = 1
             };
             {
                 Type = Pacemaker;
                 Text = getText Pacemaker;
-                Checked = false; 
+                IsChecked = false; 
                 Id = 2
             };
             {
                 Type = Registration;
                 Text = getText Registration;
-                Checked = false; 
+                IsChecked = false; 
                 Id = 3
             };
             {
                 Type = FourD;
                 Text = getText FourD;
-                Checked = false; 
+                IsChecked = false; 
                 Id = 4
             };
             {
                 Type = DIBH;
                 Text = getText DIBH;
-                Checked = false; 
+                IsChecked = false; 
                 Id = 5
             };
             {
                 Type = IMRT;
                 Text = getText IMRT;
-                Checked = false; 
+                IsChecked = false; 
                 Id = 6
             };
             {
                 Type = SRS;
                 Text = getText SRS;
-                Checked = false; 
+                IsChecked = false; 
                 Id = 7
             };
             {
                 Type = SBRT;
                 Text = getText SBRT;
-                Checked = false; 
+                IsChecked = false; 
                 Id = 8
             };
         ]
+
+    // Courses/Plans to be used in PatientSetup Screen
     type Plan =
         {
+            bindingid: string   // used for subModel bindings
             Id: string
+            IsChecked: bool       // is it checked off to be used in checklists?
         }
     type Course =
         {
             Id: string
             Plans: Plan list
         }
+
+    // PatientSetup Model
     type Model =
         {
             Courses: Course list
-            Flags: Flag list
+            Toggles: Toggle list
         }
+
+    // Initial model
     let init () =
         {
             Courses = []
-            Flags = flagList
+            Toggles = toggleList
         }
+    let initFromCourses (courses) = { init() with Courses = courses }
 
+    // Internal Messages
     type Msg =
-    | FlagChanged of int * bool
+    | ToggleChanged of int * bool
+    | UsePlanChanged of string * bool
+    | NextButtonClicked
 
-    let update msg m =
+    // Messages sent to MainWindow
+    type MainWindowMsg =
+    | NoMainWindowMsg
+    | PatientSetupCompleted of ChecklistOptions
+
+    // Handle Messages
+    let update msg (m:Model) =
         match msg with
-        | FlagChanged (id, ischecked) -> { m with Flags = m.Flags |> List.map(fun flag -> if flag.Id = id then { flag with Checked = ischecked } else flag) }
+        | ToggleChanged  (id, ischecked) -> 
+            { m with 
+                Toggles = m.Toggles 
+                |> List.map(fun toggle -> 
+                    if toggle.Id = id 
+                    then { toggle with IsChecked = ischecked } 
+                    else toggle) 
+            }, Cmd.none, NoMainWindowMsg
+        | UsePlanChanged (id, ischecked) -> 
+            { m with 
+                Courses = m.Courses 
+                |> List.map(fun course -> 
+                    { course with 
+                        Plans = course.Plans 
+                        |> List.map(fun p -> 
+                            if p.bindingid = id 
+                            then { Id = p.Id; IsChecked = ischecked; bindingid = p.bindingid } 
+                            else p) 
+                    })
+            }, Cmd.none, NoMainWindowMsg 
+        | NextButtonClicked -> m, Cmd.none, PatientSetupCompleted 
+                                                { 
+                                                    Plans = 
+                                                        [ for c in m.Courses do
+                                                            for p in c.Plans do
+                                                                if p.IsChecked then 
+                                                                    yield { PlanId = p.Id; CourseId = c.Id } ]
+                                                    Toggles = m.Toggles |> List.filter (fun t -> t.IsChecked) |> List.map (fun t -> t.Type)            
+                                                }
 
-    let flagBindings () : Binding<Model * Flag, Msg> list =
+    // WPF Bindings
+    let planBindings () : Binding<(Model * Course) * Plan, Msg> list =
         [
-            "Text" |> Binding.oneWay (fun (_, f) -> f.Text);
-            "Flag" |> Binding.twoWay ((fun (_, f:Flag) -> f.Checked), (fun value (_, f:Flag) -> FlagChanged (f.Id, value)))
+            "Id" |> Binding.oneWay (fun (_, p) -> p.Id)
+            "IsChecked" |> Binding.twoWay ((fun (_, p:Plan) -> p.IsChecked), (fun value (_, p:Plan) -> UsePlanChanged (p.bindingid, value)))
         ]
-
+    let courseBindings () : Binding<Model * Course, Msg> list =
+        [
+            "Id" |> Binding.oneWay (fun (_, c) -> c.Id)
+            "Plans" |> Binding.subModelSeq((fun (_, c) -> c.Plans), (fun (p:Plan) -> p.bindingid), planBindings)
+        ]
+    let toggleBindings () : Binding<Model * Toggle, Msg> list =
+        [
+            "Text" |> Binding.oneWay (fun (_, f) -> f.Text)
+            "IsChecked" |> Binding.twoWay ((fun (_, f:Toggle) -> f.IsChecked), (fun value (_, f:Toggle) -> ToggleChanged (f.Id, value)))
+        ]
     let bindings () : Binding<Model, Msg> list =
         [
-            "PatientSetupFlags" |> Binding.subModelSeq((fun m -> m.Flags), (fun (f:Flag) -> f.Id), flagBindings)
+            "Courses" |> Binding.subModelSeq((fun m -> m.Courses), (fun (c:Course) -> c.Id), courseBindings)
+            "PatientSetupToggles" |> Binding.subModelSeq((fun m -> m.Toggles), (fun (t:Toggle) -> t.Id), toggleBindings)
+            "PatientSetupCompleted" |> Binding.cmd NextButtonClicked
         ]
 
 module Checklist =
@@ -144,13 +217,21 @@ module Checklist =
             //OtherThingsToDisplay1: 'a option
             //OtherThingsToDisplay2: 'a option
         }
+    type PlanDetails =
+        {
+            CourseId: string
+            PlanId: string
+        }
     type Model =
         {
+            PlanDetails: PlanDetails
             Category: ChecklistCategory
             Checklist: ChecklistItem list
         }
 
 module App =
+    open PatientSetup
+    // Arguments passed from the Eclipse plugin
     type StandaloneApplicationArgs =
         {
             PatientID: string
@@ -158,33 +239,51 @@ module App =
             PlanID: string
         }
 
+    // Info displayed by the Main Window
     type SharedInfo =
         {
             PatientName: string
-            CourseID: string
-            PlanID: string
             CurrentUser: string
         }
 
+    // Status Bar at bottom of window
+    type StatusBar =
+    | None of string
+    | Indeterminate of IndeterminateStatusBar
+    | Determinate of DeterminateStatusBar
+    and IndeterminateStatusBar = { Status: string }
+    and DeterminateStatusBar = { Status: string; min: int; max: int }
+
+    // Main Model
     type Model =
         { 
             PatientSetupScreen: PatientSetup.Model
-            //ChecklistList: Checklist.Model list
+            ChecklistList: Checklist.Model list
+            ChecklistOptions: ChecklistOptions.ChecklistOptions
             SharedInfo: SharedInfo
             MainWindow: MainWindow
-            Status: string
+            StatusBar: StatusBar
             Args: StandaloneApplicationArgs
         }
 
+    // Messages
     type Msg =
         // Eclipse login
         | Login        
         | LoginSuccess of SharedInfo
         | LoginFailed of exn
+        // Patient Setup
+        | LoadCoursesIntoPatientSetup
+        | LoadCoursesSuccess of Course list
+        | LoadCoursesFailed of exn
         | PatientSetupMsg of PatientSetup.Msg
+        | LoadChecklistScreen
+
+
         | Debugton
 
     // Initial empty model
+    let readyStatus = None "Ready"
     let init (window:MainWindow) (args:StandaloneApplicationArgs) =
         { 
             PatientSetupScreen = PatientSetup.init()
@@ -192,77 +291,72 @@ module App =
                 {
                     PatientName = ""
                     CurrentUser = ""
-                    CourseID = ""
-                    PlanID = ""
                 }
+            ChecklistList = []
+            ChecklistOptions = ChecklistOptions.emptyChecklistOptions
             MainWindow = window
-            Status = ""
+            StatusBar = readyStatus
             Args = args
         }, Cmd.ofMsg Login
     
     // Initial Eclipse login function
     let login (window:MainWindow, args:StandaloneApplicationArgs) =
-
-        // Register DataContext for displaying Metro Dialogs
-        DialogParticipation.SetRegister(window, window.DataContext)
-
         async {
-            // Dialog Settings
-            let mySettings = 
-                MetroDialogSettings(
-                    NegativeButtonText = "Cancel",
-                    AnimateShow = false,
-                    AnimateHide = false,
-                    ColorScheme = MetroDialogColorScheme.Accented)
-            
-            // Display ProgressDialog
-            let! controller = DialogCoordinator.Instance.ShowProgressAsync (context = window.DataContext, title = "Logging in to Eclipse", message = "Please wait...", settings = mySettings) |> Async.AwaitTask
-            controller.SetIndeterminate()
-
             // Log in to Eclipse and get initial patient info
             try
                 do! esapi.LogInAsync()
                 do! esapi.OpenPatientAsync(args.PatientID)
                 let! patientInfo = esapi.Run(fun (pat : Patient, app : Application) ->
-                        let course = 
-                            pat.Courses 
-                            |> Seq.filter (fun x -> x.Id = args.CourseID) 
-                            |> Seq.exactlyOne
-                        let plan = 
-                            course.PlanSetups
-                            |> Seq.filter (fun x -> x.Id = args.PlanID)
-                            |> Seq.exactlyOne
                         {
                             PatientName = pat.Name
-                            CourseID = course.Id
-                            PlanID = plan.Id
                             CurrentUser = app.CurrentUser.Name
                         })
-
-                // Close Dialog
-                do! controller.CloseAsync() |> Async.AwaitTask
 
                 // Dispose of the Esapi service and Application to prevent crashing
                 window.Closed.AddHandler(fun _ _ -> esapi.Dispose())
                 
                 return LoginSuccess patientInfo
-            with ex ->
-                do! controller.CloseAsync() |> Async.AwaitTask
-
-                return LoginFailed ex
+            with ex -> return LoginFailed ex
         }
-         
+
+    let loadCoursesIntoPatientSetup () =
+        async {
+            let! courses = esapi.Run(fun (pat : Patient) ->
+                pat.Courses 
+                |> Seq.map (fun c -> { Id = c.Id; Plans = c.PlanSetups |> Seq.map(fun p -> { Id = p.Id; IsChecked = false; bindingid = c.Id + p.Id }) |> Seq.toList })
+                |> Seq.toList
+            )
+            return LoadCoursesSuccess courses
+        }
+        
 
     // Handle any messages
     let update msg m =
         match msg with
-        | Login -> { m with Status = "Logging in to Eclipse" }, Cmd.OfAsync.either login (m.MainWindow, m.Args) id LoginFailed
+        // Eclipse login
+        | Login -> { m with StatusBar = Indeterminate { Status = "Logging in to Eclipse" } }, Cmd.OfAsync.either login (m.MainWindow, m.Args) id LoginFailed
+        | LoginSuccess patientInfo -> { m with SharedInfo = patientInfo; StatusBar = readyStatus }, Cmd.ofMsg LoadCoursesIntoPatientSetup
         | LoginFailed x -> 
             System.Windows.MessageBox.Show($"{x.Message}\n\n{x.InnerException}\n\n{x.StackTrace}", "Unable to Login to Eclipse") |> ignore
-            { m with Status = "Failed to log in to Eclipse" }, Cmd.none
-        | LoginSuccess patientInfo -> { m with SharedInfo = patientInfo; Status = "Ready" }, Cmd.none
-        | PatientSetupMsg msg -> { m with PatientSetupScreen = PatientSetup.update msg m.PatientSetupScreen }, Cmd.none
-        | Debugton -> System.Windows.MessageBox.Show(m.PatientSetupScreen.Flags |> List.map(fun f -> f.Checked.ToString()) |> String.concat "\n") |> ignore; m, Cmd.none
+            { m with StatusBar = None "Failed to log in to Eclipse" }, Cmd.none
+        // Initial load of patient plans
+        | LoadCoursesIntoPatientSetup -> { m with StatusBar = Indeterminate { Status = "Loading Plans" } }, Cmd.OfAsync.either loadCoursesIntoPatientSetup () id LoadCoursesFailed
+        | LoadCoursesSuccess courses -> { m with PatientSetupScreen = initFromCourses(courses); StatusBar = None "Ready" }, Cmd.none
+        | LoadCoursesFailed x ->
+            System.Windows.MessageBox.Show($"{x.Message}\n\n{x.InnerException}\n\n{x.StackTrace}", "Unable to Load Course from Eclips") |> ignore
+            { m with StatusBar = None "Failed to load courses from Eclipse" }, Cmd.none
+        // Patient Setup Screen
+        | PatientSetupMsg patSetupMsg -> 
+            let (patSetupModel, patSetupCmd, patSetupExtraMsg) = PatientSetup.update patSetupMsg m.PatientSetupScreen
+            match patSetupExtraMsg with
+            | NoMainWindowMsg -> { m with PatientSetupScreen = patSetupModel }, Cmd.none
+            | PatientSetupCompleted options -> { m with PatientSetupScreen = patSetupModel; ChecklistOptions = options }, Cmd.ofMsg LoadChecklistScreen
+        | LoadChecklistScreen -> m, Cmd.ofMsg Debugton
+
+        | Debugton -> System.Windows.MessageBox.Show(sprintf "Plans:\n%s\n\nOptions:\n%s"
+                                    (m.ChecklistOptions.Plans |> List.map(fun p -> $"{p.PlanId} ({p.CourseId})") |> String.concat "\n")
+                                    (m.ChecklistOptions.Toggles |> List.map (fun t -> t.ToString()) |> String.concat "\n")
+                                   )|> ignore; m, Cmd.none
 
     // WPF bindings
     let bindings () : Binding<Model, Msg> list = 
@@ -270,11 +364,11 @@ module App =
             // MainWindow Info
             "PatientName" |> Binding.oneWay (fun m -> m.SharedInfo.PatientName)
             "CurrentUser" |> Binding.oneWay (fun m -> m.SharedInfo.CurrentUser)
-            "PlanID" |> Binding.oneWay (fun m -> m.SharedInfo.PlanID)
-            "CourseID" |> Binding.oneWay (fun m -> m.SharedInfo.CourseID)
-            "Status" |> Binding.oneWay (fun m -> m.Status)
+            "StatusBarStatus" |> Binding.oneWay (fun m -> match m.StatusBar with | None status -> status | Indeterminate bar -> bar.Status | Determinate bar -> bar.Status)
+            "StatusBarVisibility" |> Binding.oneWay (fun m -> match m.StatusBar with | None _ -> System.Windows.Visibility.Collapsed | _ -> System.Windows.Visibility.Visible)
+            "StatusBarIsIndeterminate" |> Binding.oneWay (fun m -> match m.StatusBar with | Determinate _ -> false | _ -> true)
 
-            // Patient Setup
+            // Patient Setup Screen
             "PatientSetup" |> Binding.subModel((fun m -> m.PatientSetupScreen), snd, PatientSetupMsg, PatientSetup.bindings)
 
             "Debugton" |> Binding.cmd Debugton
