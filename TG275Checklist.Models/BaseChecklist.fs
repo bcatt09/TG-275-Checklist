b7dfe89ae8d0bbae26a6d8e7f7601890a0bd9b08
Ben Catt <bcatt09@gmail.com>
2021-04-20 15:12:51 -0400

    Fixing BaseChecklist merge

--- TG275Checklist.Models/BaseChecklist.fs
+++ TG275Checklist.Models/BaseChecklist.fs
@@ -8,7 +8,7 @@ module BaseChecklist =
 
     let prescriptionChecklist =
         [
-            "Prescription (with respect to standard of care, institutional clinical guidelines or clinical trial is applicable)", None
+            "Prescription (with respect to standard of care, institutional clinical guidelines or clinical trial is applicable)", Some getFullPrescription
             "Final plan and prescription approval by physician", Some getPrescriptionVsPlanApprovals
             "Site and laterality (incl. medical chart to confirm laterality)", None
             "Prescription vs consult note (e.g. physician report in EMR on plans for treatment)", None
@@ -16,15 +16,15 @@ module BaseChecklist =
             "Fractionation pattern and regimen (e.g. daily, BID, Quad Shot, regular plan follow by boost, etc.)", None
             "Energy matches prescription", Some getPrescriptionVsPlanEnergy
             "Modality (e.g. electrons, photons, protons, etc.)", Some getPrescriptionVsPlanModality
-            "Technique (e.g. 3D, IMRT, VMAT, SBRT, etc.) matches prescription", None
-            "Bolus", None
+            "Technique (e.g. 3D, IMRT, VMAT, SBRT, etc.) matches prescription", Some getPrescriptionVsPlanTechnique
+            "Bolus", Some getPrescriptionVsPlanBolus
             "Additional shielding (e.g. eye shields, testicular shields, etc. as applicable)", None
-            "Course intent/diagnosis", None
+            "Course intent/diagnosis", Some getCoursePlanIntentDiagnosis
         ] |> createCategoryChecklist Prescription
 
     let simulationChecklist =
         [
-            "Physician directive for imaging technique, setup and immobilization (this may include: contrast, scanning orientation, immobilization device, etc.)", Some testFunction
+            "Physician directive for imaging technique, setup and immobilization (this may include: contrast, scanning orientation, immobilization device, etc.)", None
             "Description of target location on physician planning directive (e.g. RUL Lung, H&N, L1‐L4)", None
             "Patient set up, positioning and immobilization*: (a) Appropriate for site and/or per clinical standard procedures, (b) Written or photographic documentation of patient positioning, immobilization and ancillary devices, including setup note", None
             "Image quality and usability: CT Scan Artifacts, Scan sup/inf Range Includes Enough Data, Scan FOV encompasses all required information, Use of Contrast", None
@@ -36,10 +36,10 @@ module BaseChecklist =
 
     let contouringChecklist =
         [
-            "Target(s)* ‐ e.g. discernible errors, missing slices, mislabeling, gross anatomical deviations.", Some testFunction
+            "Target(s)* ‐ e.g. discernible errors, missing slices, mislabeling, gross anatomical deviations.", Some getTargetInfo
             "Organs‐at‐risk (OAR's)", None
             "PTV and OAR Margin ‐ as specified in the chart and/or per protocol", None
-            "Body/External contour", None
+            "Body/External contour", Some getBodyInfo
             "Density overrides applied as needed (ex. High‐Z material, contrast, artifacts, etc.)", None
             "Consideration of Supporting Structures (i.e. couch, immobilization and ancillary devices, etc.)", None
             "Approval of contours by MD", None
@@ -47,7 +47,7 @@ module BaseChecklist =
 
     let standardOperatingProceduresChecklist =
         [
-            "Course and plan ID", Some testFunction
+            "Course and plan ID", None
             "Treatment technique (e.g. 3D, IMRT, VMAT, SBRT, etc.)", None
             "Delivery system (e.g. standard linac, CyberKnife, Tomotherapy, etc. as applicable)", None
             "Beam arrangement", None
@@ -68,14 +68,14 @@ module BaseChecklist =
 
     let planQualityChecklist =
         [
-            "Target coverage and target planning objectives", Some testFunction
+            "Target coverage and target planning objectives", None
             "Sparing of OARs and OAR planning objectives", None
             "Plan conforms to clinical trial (as applicable)", None
             "Structures used during optimization", None
             "Physician designed apertures", None
             "Dose distribution", None
             "Hot spots", None
-            "Reference points", None
+            "Reference points", Some getReferencePoints
             "Plan normalization", None
             "Calculation algorithm and calculation grid size", None
             "Prior Radiation accounted for in plan", None
@@ -84,21 +84,21 @@ module BaseChecklist =
        
     let doseVerificationChecklist =
         [
-            "Second calculation check and/or QA performed", Some testFunction
+            "Second calculation check and/or QA performed", None
             "Verification plan for patient specific QA measurement", None
             "Request for in‐vivo dosimetry", None
         ] |> createCategoryChecklist Verification
         
     let isocenterChecklist =
         [
-            "Isocenter: placement and consistency between patient marking and setup instructions", Some testFunction
+            "Isocenter: placement and consistency between patient marking and setup instructions", None
             "Additional shifts", None
             "Multiple isocenters", None
         ] |> createCategoryChecklist Isocenter
         
     let imageGuidanceChecklist =
         [
-            "Matching instructions (e.g. 2D/2D, 3D, etc.) and MD directive for IGRT", Some testFunction
+            "Matching instructions (e.g. 2D/2D, 3D, etc.) and MD directive for IGRT", None
             "Matching structures", None
             "Reference CT", None
             "Isocenter on reference image(s), 2D or 3D", None
@@ -112,8 +112,8 @@ module BaseChecklist =
         
     let schedulingChecklist =
         [
-            "Plan is scheduled for treatment", None
-            "Scheduling of safety‐critical tasks (e.g. weekly chart checks, IMRT QA, etc.)", Some testFunction
+            "Plan is scheduled for treatment", Some getPlanScheduling
+            "Scheduling of safety‐critical tasks (e.g. weekly chart checks, IMRT QA, etc.)", None
         ] |> createCategoryChecklist Scheduling
         
     let replanChecklist =
