f9f034e7edf663b00a4a162d271e9b099da46e31
Ben Catt <bcatt09@gmail.com>
2021-05-05 10:26:16 -0400

    Fixed error when CT ID is less than 8 characters

--- TG275Checklist.Models/EsapiCalls.fs
+++ TG275Checklist.Models/EsapiCalls.fs
@@ -405,11 +405,15 @@ Other Linked Plans: {linkedPlans}")
                 match ct.CreationDateTime.HasValue with
                 | true -> ct.CreationDateTime.Value.ToShortDateString()
                 | false -> warn "No creation date"
+            let tryGetDate (id: string) =
+                if id.Length < 8
+                then sprintf "%8s" id
+                else id.Substring(0,8)
             let isDateFunction = // If the CT ID is a date we'll highlight is red if it doesn't match, otherwise we'll use yellow
-                if fst (DateTime.TryParse(ct.Id.Substring(0,8)))
+                if fst (DateTime.TryParse(tryGetDate(ct.Id)))
                 then getPassFail
                 else getPassWarn
-            let dateResult = isDateFunction (ct.Id.Substring(0,8) = DateTime.Parse(creationDate).ToString("yyyyMMdd"))
+            let dateResult = isDateFunction (tryGetDate(ct.Id) = DateTime.Parse(creationDate).ToString("yyyyMMdd"))
 
             $"Imaging Device for HU Curve: {ct.Series.ImagingDeviceId}\nCT Name: {dateResult ct.Id}\nCreation date: {dateResult creationDate}\n# of slices: {ct.ZSize}"
         |> stringOutput
